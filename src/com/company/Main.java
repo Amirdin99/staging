d36d8a27c9f32d655466d605890ef6ababcad422
Amirdin <amirdinamirdin1999@gmail.com>
2020-11-22 12:18:17 +0300

    Just

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+         Shape shape=new Shape();
+         shape.show();
+         shape.showPeremetr();
+
+
+    }
+}
---
+++ src/com/company/Shape.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Shape {
+    String name ="Anvar";
+    String color="qora";
+    int peremetr=20;
+    int square =100;
+    void show(){
+        System.out.println("Parametrlari:"+"\n"+name+"\n" +color+"\n"+peremetr+"\n "+square);
+    }
+    void showPeremetr(){
+        System.out.println("permetr:"+peremetr);
+    }
+}
